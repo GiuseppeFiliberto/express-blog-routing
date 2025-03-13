44ce53d882c75c71d96e1c2fa1d1d523c7bd0868
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-03-13 17:23:48 +0100

    add main route on the main file

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require ('express')
+const app = express();
+const port = 3000;
+
+//server listening
+app.listen(port, () =>{
+    console.log(`You're running the server on http://localhost:${port}`)
+})
+
+//main route
+app.get('/', (req,res) =>{
+    console.log("welcome to the server");
+    
+});
+
