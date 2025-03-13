af52592747f4fd345564ac9f62b73dbd56778620
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-03-13 18:06:22 +0100

    add all posts routes

--- routers/posts.js
+++ routers/posts.js
@@ -3,9 +3,52 @@ const router = express.Router();
 
 //index
 
-router.get('/api/v1/posts', (req,res) =>{
+router.get('/', (req,res) =>{
 
     res.send("All posts here");
+
+})
+
+// show 
+router.get('/:id', (req,res) =>{
+
+    const postsId = req.params.id
+    res.send(`this is the post: ${postsId}`);
+
+})
+
+//store
+
+router.post('/', (req,res) =>{
+
+    res.send("All posts here");
+
+})
+
+// update
+
+router.put('/:id', (req,res) =>{
+
+    const postsId = req.params.id
+    res.send(`this is the post: ${postsId}`);
+
+})
+
+// modify
+
+router.patch('/:id', (req,res) =>{
+
+    const postsId = req.params.id
+    res.send(`this is the post: ${postsId}`);
+
+})
+
+// delete
+
+router.delete('/:id', (req,res) =>{
+
+    const postsId = req.params.id
+    res.send(`this is the post: ${postsId}`);
 
 })
 
